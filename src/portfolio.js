f0b9928679c40488bdebb30908864479add58eb4
chrisdcruz1 <christopherdcruzcoc@gmail.com>
2025-05-15 22:00:23 +0530

    name change

--- src/portfolio.js
+++ src/portfolio.js
@@ -21,7 +21,7 @@ const illustration = {
 
 const greeting = {
   username: "Saad Paratha",
-  title: "Hi all, I'm CudaBit",
+  title: "Greetings! We're CudaBit",
   subTitle: emoji(
     "A passionate Full Stack Software Developer 🚀 having an experience of building Web and Mobile applications with JavaScript / Reactjs / Nodejs / React Native and some other cool libraries and frameworks."
   ),
