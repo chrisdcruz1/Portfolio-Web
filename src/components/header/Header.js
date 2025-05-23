04b9eb32d694d825fdb90b59b25532d3a722011b
chrisdcruz1 <christopherdcruzcoc@gmail.com>
2025-05-23 21:16:37 +0530

    content changes

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -35,7 +35,7 @@ function Header() {
           <img
             src={logoImage}
             alt="CudaBit"
-            style={{height: "60px", verticalAlign: "middle"}}
+            style={{height: "120px", verticalAlign: "middle"}}
           />
           {/* <span className="grey-color">/&gt;</span> */}
         </a>
