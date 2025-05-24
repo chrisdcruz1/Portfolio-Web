de87fa020154885f249205ea30b90f50809f2908
chrisdcruz1 <christopherdcruzcoc@gmail.com>
2025-05-24 17:17:26 +0530

    content changes

--- src/containers/greeting/Greeting.js
+++ src/containers/greeting/Greeting.js
@@ -38,7 +38,7 @@ export default function Greeting() {
               <div id="resume" className="empty-div"></div>
               <SocialMedia />
               <div className="button-greeting-div">
-                <Button text="Contact me" href="#contact" />
+                <Button text="Contact Us" href="#contact" />
                 {greeting.resumeLink && (
                   <a
                     href={require("./resume.pdf")}
