190eb2d7683f97c066c9cce314df3eef617709d1
chrisdcruz1 <christopherdcruzcoc@gmail.com>
2025-05-24 17:22:59 +0530

    content changes

--- src/portfolio.js
+++ src/portfolio.js
@@ -406,7 +406,7 @@ const contactInfo = {
   title: emoji("Contact Me ☎️"),
   subtitle:
     "Discuss a project or just want to say hi? My Inbox is open for all.",
-  number: "+92-0000000000",
+  number: "+91-7250820825",
   email_address: "cudabit@cudabit.live"
 };
 
